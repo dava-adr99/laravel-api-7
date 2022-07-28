16e03dc75f2330ea8449183264a9771538c22d78
alfianfalahudin <74964771+alfianfalahudin@users.noreply.github.com>
2022-07-28 18:27:25 +0700

    login logut halaman admin OK

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function(){
+    Route::get('/', 'Admin\DashboardController@index');
+});
