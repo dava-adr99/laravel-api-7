f0a69f6f3d89f7f43ab82b7e52d54f56ae34ab18
alfianfalahudin <74964771+alfianfalahudin@users.noreply.github.com>
2022-07-29 09:32:10 +0700

    tambah Auth register dan route

---
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validateData =$request->validate([
+            'nama'=>'required | max:25',
+            'email'=>'email | required | unique:users',
+            'password'=>'required | confirmed'
+        ]);
+
+        //create user
+        $user = new User([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+
+        $user->save;
+
+        return response()->json($user, 201);
+    }
+}
